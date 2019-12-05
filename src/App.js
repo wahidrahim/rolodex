fda7ea3968492275e44bc9d137f2205a5def6b19
Wahid Rahim <wahidrahim@gmail.com>
2019-12-04 23:32:45 -0500

    20 - class components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  newString = 'testing'
+
+  constructor() {
+    super();
+
+    this.state = {
+      greeting: "Hello World"
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.greeting}</p>
+          <button
+            onClick={() => {
+              this.setState({ greeting: this.newString }, () => {
+                console.log("test");
+              });
+            }}
+          >
+            change string
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
