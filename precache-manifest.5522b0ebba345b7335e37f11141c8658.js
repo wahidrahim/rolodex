6429a28716c00e4c297de7d2f2575d3a1804ee46
Wahid Rahim <wahidrahim@gmail.com>
2019-12-16 21:49:07 -0500

    Updates

---
+++ precache-manifest.5522b0ebba345b7335e37f11141c8658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe84edfbb963ae54e09cbc845a3f4159",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "ee0bac6c26a9c814e6cc",
+    "url": "/rolodex/static/css/main.6dace1c3.chunk.css"
+  },
+  {
+    "revision": "0c0d218d604853eeba03",
+    "url": "/rolodex/static/js/2.b687fe75.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/rolodex/static/js/2.b687fe75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee0bac6c26a9c814e6cc",
+    "url": "/rolodex/static/js/main.d0196737.chunk.js"
+  },
+  {
+    "revision": "d72009913ef41833d3a3",
+    "url": "/rolodex/static/js/runtime-main.2fc8d5fe.js"
+  }
+]);
